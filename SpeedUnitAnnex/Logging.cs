4bdfa80a4b7fac7f747af57fafa006e4e4c889a3
Alexander Yalov <alexander.yalov@gmail.com>
2019-03-19 22:55:23 +0300

    update Logging

--- SpeedUnitAnnex/Logging.cs
+++ SpeedUnitAnnex/Logging.cs
@@ -5,16 +5,16 @@ namespace SpeedUnitAnnex
 {
     public static class Logging
     {
-        private static readonly string PREFIX = "<color=green>SpeedUnitAnnex:</color> " ;
+        private static readonly string PREFIX = "<color=green>[SpeedUnitAnnex]</color> " ;
 
         public static void Log<T>(T msg)
         {
-            Debug.Log(PREFIX + DateTime.Now.ToString("hh:mm:ss.f ") + msg.ToString());
+            Debug.Log(PREFIX + DateTime.Now.ToString("HH:mm:ss.f ") + msg.ToString());
         }
 
         public static void Log(string msg, params object[] arg)
         {
-            Debug.Log(PREFIX + DateTime.Now.ToString("hh:mm:ss.f ") + String.Format(msg, arg));
+            Debug.Log(PREFIX + DateTime.Now.ToString("HH:mm:ss.f ") + String.Format(msg, arg));
         }
     }
 }
