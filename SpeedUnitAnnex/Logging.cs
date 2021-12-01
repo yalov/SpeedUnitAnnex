bd3f4022112e74163dd89a6977fd03959173556f
Alexander Yalov <alexander.yalov@gmail.com>
2021-12-01 22:34:43 +0300

    change min ksp

--- SpeedUnitAnnex/Logging.cs
+++ SpeedUnitAnnex/Logging.cs
@@ -7,7 +7,20 @@ namespace SpeedUnitAnnex
     public static class Logging
     {
         private static readonly string PREFIX = "<color=green>[SpeedUnitAnnex]</color> ";
+        internal static String _AssemblyName { get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Name; } }
         private static readonly bool time = false;
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void LogDebug(params object[] args)
+        {
+            Log(args);
+        }
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void LogFormatDebug(string msg, params object[] args)
+        {
+            LogFormat(msg, args);
+        }
 
         public static void Log(params object[] args)
         {
